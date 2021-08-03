d66d619d96634190f30fd332e3eb114ac2789d90
Ubuntu <ajay.u@knowlarity.com>
2021-08-03 17:11:35 +0530

    book movie

---
+++ client/src/layouts/PublicLayout/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Divider, Typography, Link } from '@material-ui/core';
+import useStyles from './styles';
+
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Divider />
+      <Typography className={classes.copyright} variant="body1">
+        &copy; Gourav Kumar
+      </Typography>
+      <Typography variant="caption">
+         |{' '}
+        <Link href="/" target="_blank" rel="noopener">
+        Gourav Kumar
+        </Link>
+      </Typography>
+    </div>
+  );
+}
